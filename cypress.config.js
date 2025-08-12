7ad88c734d3e12e63671f1080ae07a59f297d412
Gabriela Silveira <137567010+Gabrielaprog@users.noreply.github.com>
2025-08-12 01:28:02 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080', // endereço do servidor local
+    baseUrl: 'http://localhost:8081', // endereço do servidor local
     setupNodeEvents(on, config) {
       // eventos do Node, se precisar
     },
