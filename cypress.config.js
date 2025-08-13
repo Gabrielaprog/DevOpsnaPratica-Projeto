50495152334a923ac63b714251aea3dab69c4792
Gabriela <gabrielasilveira309@gmail.com>
2025-08-13 10:50:27 -0300

    Atualizando arquivos de teste cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080', // endereço do servidor local
+    baseUrl: 'http://192.168.0.62:8080', // endereço do servidor local
     setupNodeEvents(on, config) {
       // eventos do Node, se precisar
     },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,3 @@
-
 describe('To-Do List', () => {
   beforeEach(() => {
     cy.visit('/index.html'); // Abrindo seu arquivo local
