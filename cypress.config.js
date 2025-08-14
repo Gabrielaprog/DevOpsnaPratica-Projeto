5a0dc7fa32691d0c501503da911a2904a13bd748
Gabriela <gabrielasilveira309@gmail.com>
2025-08-13 21:09:02 -0300

    Retirando arquivos de exemplo de teste cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8080', // endereço do servidor local
+    baseUrl: 'http://127.0.0.1:8080', 
+    // endereço do servidor local
     setupNodeEvents(on, config) {
       // eventos do Node, se precisar
     },
