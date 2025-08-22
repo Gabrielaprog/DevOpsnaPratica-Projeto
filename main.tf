528766f292e50e8c2d44554d104b641a9818978f
Gabriela <gabrielasilveira309@gmail.com>
2025-08-22 16:09:50 -0300

    Alterações nos arquivos main.tf e main.yml

--- main.tf
+++ main.tf
@@ -12,9 +12,6 @@ provider "local" {}
 # Simula um recurso criando um arquivo
 resource "local_file" "app_config" {
   filename = "${path.module}/app_config.txt"
-  content  = <<EOT
-Aplicação provisionada com sucesso!
-Ambiente: Desenvolvimento
-Pipeline CI executada.
-EOT
+  content  = Aplicação provisionada com sucesso!
+
 }
